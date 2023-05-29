c62f2b1556aba62c1f42017dc20c00e17c7cdabf
Jazzband <medey90@mail.ru>
2023-05-29 15:42:20 +0300

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Jazzband on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
