aa96df8fd113790e65ff7b3855cfbd90aa3b89ed
Nikolay <103628537+Nike90210@users.noreply.github.com>
2023-07-16 16:48:34 +0300

    all ratings saving and multiple screen features were added

--- HIITFit/View/ExerciseView.swift
+++ HIITFit/View/ExerciseView.swift
@@ -9,91 +9,91 @@ import SwiftUI
 import AVKit
 
 struct ExerciseView: View {
-  @EnvironmentObject var history: HistoryStore
-  @State private var rating = 0
-  @State private var showHistory = false
-  @State private var showSuccess = false
-  @State private var timerDone = false
-  @State private var showTimer = false
+    @EnvironmentObject var history: HistoryStore
 
-  @Binding var selectedTab: Int
-  let index: Int
+    @State private var showHistory = false
+    @State private var showSuccess = false
+    @State private var timerDone = false
+    @State private var showTimer = false
 
-  var exercise: Exercise {
-    Exercise.exercises[index]
-  }
-  var lastExercise: Bool {
-    index + 1 == Exercise.exercises.count
-  }
+    @Binding var selectedTab: Int
+    let index: Int
 
-  var startButton: some View {
-    Button("Start Exercise") {
-      showTimer.toggle()
+    var exercise: Exercise {
+        Exercise.exercises[index]
     }
-  }
-
-  var doneButton: some View {
-    Button("Done") {
-      history.addDoneExercise(Exercise.exercises[index].exerciseName)
-      timerDone = false
-      showTimer.toggle()
-      if lastExercise {
-        showSuccess.toggle()
-      } else {
-        selectedTab += 1
-      }
+    var lastExercise: Bool {
+        index + 1 == Exercise.exercises.count
     }
-  }
 
-  var body: some View {
-    GeometryReader { geometry in
-      VStack(spacing: 0) {
-        HeaderView(
-          selectedTab: $selectedTab,
-          titleText: Exercise.exercises[index].exerciseName)
-          .padding(.bottom)
+    var startButton: some View {
+        Button("Start Exercise") {
+            showTimer.toggle()
+        }
+    }
 
-        VideoPlayerView(videoName: exercise.videoName)
-          .frame(height: geometry.size.height * 0.45)
-
-        HStack(spacing: 150) {
-          startButton
-          doneButton
-            .disabled(!timerDone)
-            .sheet(isPresented: $showSuccess) {
-              SuccessView(selectedTab: $selectedTab)
-                .presentationDetents([.medium, .large])
+    var doneButton: some View {
+        Button("Done") {
+            history.addDoneExercise(Exercise.exercises[index].exerciseName)
+            timerDone = false
+            showTimer.toggle()
+            if lastExercise {
+                showSuccess.toggle()
+            } else {
+                selectedTab += 1
             }
         }
-        .font(.title3)
-        .padding()
-
-        if showTimer {
-          TimerView(
-            timerDone: $timerDone,
-            size: geometry.size.height * 0.07)
-        }
-
-        Spacer()
-        RatingView(rating: $rating)
-          .padding()
-
-        Button("History") {
-          showHistory.toggle()
-        }
-        .sheet(isPresented: $showHistory) {
-          HistoryView(showHistory: $showHistory)
-        }
-          .padding(.bottom)
-      }
     }
-  }
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                HeaderView(
+                    selectedTab: $selectedTab,
+                    titleText: Exercise.exercises[index].exerciseName)
+                .padding(.bottom)
+
+                VideoPlayerView(videoName: exercise.videoName)
+                    .frame(height: geometry.size.height * 0.45)
+
+                HStack(spacing: 150) {
+                    startButton
+                    doneButton
+                        .disabled(!timerDone)
+                        .sheet(isPresented: $showSuccess) {
+                            SuccessView(selectedTab: $selectedTab)
+                                .presentationDetents([.medium, .large])
+                        }
+                }
+                .font(.title3)
+                .padding()
+
+                if showTimer {
+                    TimerView(
+                        timerDone: $timerDone,
+                        size: geometry.size.height * 0.07)
+                }
+
+                Spacer()
+                RatingView(exerciseIndex: index)
+                    .padding()
+
+                Button("History") {
+                    showHistory.toggle()
+                }
+                .sheet(isPresented: $showHistory) {
+                    HistoryView(showHistory: $showHistory)
+                }
+                .padding(.bottom)
+            }
+        }
+    }
 }
 
 struct ExerciseView_Previews: PreviewProvider {
-  static var previews: some View {
-    ExerciseView(selectedTab: .constant(0), index: 0)
-      .environmentObject(HistoryStore())
-  }
+    static var previews: some View {
+        ExerciseView(selectedTab: .constant(0), index: 0)
+            .environmentObject(HistoryStore())
+    }
 }
 
--- HIITFit/View/RatingView.swift
+++ HIITFit/View/RatingView.swift
@@ -8,30 +8,71 @@
 import SwiftUI
 
 struct RatingView: View {
-  @Binding var rating: Int
-  let maximumRating = 5
+    @AppStorage("ratings") private var ratings = ""
+    @State private var rating = 0
+    let maximumRating = 5
+    let exerciseIndex: Int
 
-  let onColor = Color.red
-  let offColor = Color.gray
+    let onColor = Color.red
+    let offColor = Color.gray
 
-  var body: some View {
-    HStack {
-      ForEach(1 ..< maximumRating + 1, id: \.self) { index in
-        Image(systemName: "waveform.path.ecg")
-          .foregroundColor(
-            index > rating ? offColor : onColor)
-          .onTapGesture {
-            rating = index
-          }
-      }
+    init( exerciseIndex: Int) {
+
+        self.exerciseIndex = exerciseIndex
+        let desiredLength = Exercise.exercises.count
+        if ratings.count < desiredLength {
+            ratings = ratings.padding(
+                toLength: desiredLength,
+                withPad: "0",
+                startingAt: 0)
+        }
     }
-    .font(.largeTitle)
-  }
+
+    fileprivate func convertRating() {
+        let index = ratings.index(
+            ratings.startIndex,
+            offsetBy: exerciseIndex)
+        let character = ratings[index]
+        rating = character.wholeNumberValue ?? 0
+    }
+
+    var body: some View {
+        HStack {
+            ForEach(1 ..< maximumRating + 1, id: \.self) { index in
+                Image(systemName: "waveform.path.ecg")
+                    .onChange(of: ratings){ _ in
+                        convertRating()
+                    }
+                    .foregroundColor(
+                        index > rating ? offColor : onColor)
+                    .onTapGesture {
+                        updateRating(index: index)
+
+                    }
+            }
+        }
+        .font(.largeTitle)
+        .onAppear {
+            convertRating()
+        }
+    }
+
+    func updateRating(index: Int){
+        rating = index
+        let index = ratings.index(
+            ratings.startIndex,
+            offsetBy: exerciseIndex)
+        ratings.replaceSubrange(index...index, with: String(rating))
+    }
 }
 
 struct RatingView_Previews: PreviewProvider {
-  static var previews: some View {
-    RatingView(rating: .constant(3))
-      .previewLayout(.sizeThatFits)
-  }
+    // Optional UserDefalt для Preview
+    @AppStorage("ratings") static var ratings: String?
+    static var previews: some View {
+        // что бы удалить ключ UserDefalt с Preview нужно присвоить ему значение nil, а nil может быть только в Optional
+        ratings = nil
+        return RatingView(exerciseIndex: 0)
+            .previewLayout(.sizeThatFits)
+    }
 }
